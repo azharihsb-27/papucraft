828bf250a30c701c8d80d78c7806c2028b50a0a9
mrakasondara <rakasondara21@gmail.com>
2024-05-31 15:41:43 +0700

    add endpoint register

--- api/src/firebase/model/user.js
+++ api/src/firebase/model/user.js
@@ -13,12 +13,19 @@ const rootReference = ref(database);
 
 const getDetailUser = async (id) => {
   const dbGet = await get(child(rootReference, `user/${id}`));
-  return dbGet.val(); 
+  return dbGet.val();
 };
 
-const addUser = async (body) =>{
-  const {username} = body
-  return username
-}
+const addUser = (body) => {
+  const { username, email } = body;
+  const data = {
+    username,
+    email,
+    profile_image: 'profile.jpg',
+  };
+  const reference = child(rootReference, "user/" + 84);
+  set(reference, data);
+  return email;
+};
 
 module.exports = { getDetailUser, addUser };
