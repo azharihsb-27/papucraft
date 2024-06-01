3ecedb65a54fd1418eb824224b24ae61ff51cef2
mrakasondara <rakasondara21@gmail.com>
2024-06-01 16:06:59 +0700

    add function get and add artikel

--- client/src/scripts/utils/api.js
+++ client/src/scripts/utils/api.js
@@ -6,17 +6,38 @@ const getAllKebudayaan = async () => {
   return responseJson;
 };
 
-const addUser = async ({usernameValue, emailValue, uid}) =>{
-  const data = {usernameValue,emailValue,uid}
-  await fetch(`${baseURL}/register`, {
-    method: 'POST',
+const getAllArtikel = async () => {
+  const response = await fetch(`${baseURL}/kebudayaan`);
+  const responseJson = await response.json();
+  return responseJson;
+};
+
+const addUser = async ({ usernameValue, emailValue, uid }) => {
+  const data = { usernameValue, emailValue, uid };
+  const response = await fetch(`${baseURL}/register`, {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
-  })
-  const responseJson = await response.json()
-  return responseJson
-}
+  });
+  const responseJson = await response.json();
+  return responseJson;
+};
 
-module.exports = { getAllKebudayaan, addUser };
+const addArtikel = async ({ judul, source, file, body }) => {
+  const data = new FormData();
+  data.set("judul", judul);
+  data.set("source", source);
+  data.set("file", file[0]);
+  data.set("body", body);
+
+  const response = await fetch(`${baseURL}/artikel`, {
+    method: "POST",
+    body: data,
+  });
+  const responseJson = await response.json();
+  return responseJson;
+};
+
+module.exports = { getAllKebudayaan, addUser, getAllArtikel, addArtikel };
