92d50962ec867b6ba211e56115cd8b3b8bcdd47f
qurtubi <qurtubi1411@gmail.com>
2024-06-16 20:13:41 +0700

    update course

--- client/src/scripts/views/pages/course.js
+++ client/src/scripts/views/pages/course.js
@@ -1,4 +1,4 @@
-import { getAllKelas } from '../../utils/api';
+import { getAllKelas } from "../../utils/api";
 
 const Course = {
   async render() {
@@ -8,15 +8,7 @@ const Course = {
         <!-- Kelas Populer -->
         <article class="p-7 lg:p-14">
             <h2 class="text-xl font-semibold mb-2 pb-5 text-primary">Kelas Populer</h2>
-            <div class="flex items-center bg-white shadow-lg rounded-lg p-4 mb-6">
-                <div class="w-1/2 bg-gray-300 h-100 flex justify-center items-center overflow-hidden">
-                    <img data-src="/img/bg.jpg" alt="Kelas Populer" class="lazyload object-cover h-full w-full"/>
-                </div>
-                <div class="w-2/3 px-10">
-                    <h3 class="text-lg font-bold text-primary pb-2" >Nama Kelas</h3>
-                    <p class="text-gray-600">Lorem ipsum dolor sit amet consectetur. Tempor nibh tortor mattis rutrum lobortis. Ut eleifend rhoncus natoque ipsum mattis. Non non est arcu purus vel elit pulvinar blandit lobortis.</p>
-                    <button class="mt-4 px-4 py-2 bg-red-600 text-white font-semibold rounded hover:bg-primary_dark duration-300"><a href="#/course/:id">Lihat Selengkapnya</a></button>
-                </div>
+            <div class="flex flex-col md:flex-row items-center bg-white shadow-lg rounded-lg p-4 mb-6" id="kelas-populer">
             </div>
         </article>
 
@@ -33,9 +25,30 @@ const Course = {
   },
 
   async afterRender() {
-    const container = document.getElementById('semuaKelas');
+    const container = document.getElementById("semuaKelas");
+    const populer = document.getElementById("kelas-populer");
     const { data } = await getAllKelas();
     console.log(data);
+    const kelasPopuler = data.sort((a, b) => {
+      return b.views - a.views;
+    });
+    const getKelasPopuler = kelasPopuler.slice(0, 1);
+    console.log(getKelasPopuler);
+    if (populer) {
+      populer.innerHTML += getKelasPopuler.map((kelas) => {
+        return `
+      <div class="w-full md:w-1/2 bg-gray-300 h-64 flex justify-center items-center overflow-hidden rounded-t-lg md:rounded-t-none md:rounded-l-lg">
+        <img data-src="${kelas.thumbnail}" alt="Kelas Populer" class="lazyload object-cover h-full w-2/4"/>
+        </div>
+    <div class="w-full md:w-1/2 p-4 md:px-10">
+      <h3 class="text-lg font-bold text-primary pb-2" >${kelas.nama_kelas}</h3>
+      <p class="text-gray-600">${kelas.deskripsi}</p>
+      <button class="mt-4 px-4 py-2 bg-red-600 text-white font-semibold rounded hover:bg-primary_dark duration-300"><a href="#/course/${kelas.id}">Lihat Selengkapnya</a></button>
+  </div>
+      `;
+      });
+    }
+    console.log(kelasPopuler);
     if (container) {
       container.innerHTML += data
         .map((kelas) => {
@@ -52,7 +65,7 @@ const Course = {
         </div>
             `;
         })
-        .join('');
+        .join("");
     }
   },
 };
